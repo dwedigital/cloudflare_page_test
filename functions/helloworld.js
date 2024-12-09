5bc9aedd213acefc972ea2666a7ec2c56792dfe5
Dave Edwards <dave@dwedigital.com>
2024-12-09 13:29:47 +0000

    json

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,3 @@
 export function onRequest(context){
-  return new Response({'message':'Hey there! This is a simple response from a Cloudflare Worker.'})
+  return new Response(JSON.stringify({'message':'Hey there! This is a simple response from a Cloudflare Worker.'}))
 }
